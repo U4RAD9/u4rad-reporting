9c2beae63025d33cfa7d1da2fae4f1eb1c42a916
Aman Gupta <amanguptadbg1229@gmail.com>
2023-10-07 17:08:09 +0530

    views update

--- authsysproject/users/views.py
+++ authsysproject/users/views.py
@@ -28,7 +28,7 @@ from django.shortcuts import HttpResponse
 from django.views import View
 import os
 import pickle
-from googleapiclient.discovery import build
+from apiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 import io
